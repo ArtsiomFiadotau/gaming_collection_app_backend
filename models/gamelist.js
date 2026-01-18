a52028b48597b80f309dfc4201c54563339b2fae
Artyom Fedotov <hahahappyface@gmail.com>
2026-01-18 13:35:45 +0300

    16 gamelist.js hasMany for listitem

--- models/gamelist.js
+++ models/gamelist.js
@@ -3,6 +3,7 @@ import { Model } from 'sequelize';
 export default (sequelize, DataTypes) => {
   class GameList extends Model {
     static associate(models) {
+      GameList.hasMany(models.ListItem, { foreignKey: 'listId'});
       GameList.belongsTo(models.User, { foreignKey: 'userId' });
       GameList.belongsToMany(models.Game, {
         through: models.ListItem, // ссылка на модель, не строку
